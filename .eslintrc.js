22f2a7c5e215577dc9731c5535f4a49f9a38c53a
Neeraj Dalal <nd941z@gmail.com>
2021-05-10 16:13:28 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
   extends: [
     'airbnb-base',
     'eslint:recommended',
@@ -7,8 +12,13 @@ module.exports = {
     'prettier/@typescript-eslint',
   ],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    semi: ['never'],
+    'import/extensions': 'off',
+    'no-console': 'off',
+    semi: ['error', 'never'],
   },
 }
